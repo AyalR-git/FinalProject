64a5505f176bb3332b519b23b7731f210be158ca
Ayal Rana <ranaayal94@gmail.com>
2021-10-02 14:32:28 +0300

    [TripPlanner] Added the new modules that will be used by the TripPlanner obj.
    Signed-off-by: Ayal Rana <ranaayal94@gmail.com>

---
+++ Modules/map_ops.py
@@ -0,0 +1,17 @@
+"""
+Written by Ayal Rana & Nir Presser
+"""
+
+from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import routing_enums_pb2
+
+
+class MapOps(object):
+    """
+    The google maps operations wrapper object
+    """
+    def __init__(self):
+        """
+        Initiats the object
+        """
+        pass
---
+++ Modules/solver.py
@@ -0,0 +1,26 @@
+"""
+Written by Ayal Rana & Nir Presser
+"""
+
+from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import routing_enums_pb2
+
+
+class Solver(object):
+    """
+    The solver object
+    """
+
+    def __init__(self):
+        """
+        Initiats the object
+        """
+        pass
+
+    def solve(self, model):
+        """
+        Solves the problem according to the input
+        :param model: The model to solve
+        :return:
+        """
+        pass
